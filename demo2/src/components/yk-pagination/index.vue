62a161b6104c1356c18e6eaa60ad2a978f0b0fb3
lifangfanga <82084425+lifangfanga@users.noreply.github.com>
2021-08-26 09:49:52 +0800

    Create index.vue

---
+++ demo2/src/components/yk-pagination/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="block">
+    <el-pagination :page-sizes="pageSizes"
+                   :current-page.sync="currentPage"
+                   :layout="layout"
+                   :total="total"
+                   @size-change="handleSizeChange"
+                   @current-change="handleCurrentChange">
+    </el-pagination>
+  </div>
+</template>
+<script>
+export default {
+  name: 'yk-pagination',
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    // 每页展示的条数
+    pageSizes: {
+      type: Array,
+      default () {
+        return [20, 50, 100]
+      }
+    },
+    total: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      layout: 'total, sizes, prev, pager, next, jumper'
+    }
+  },
+  methods: {
+    handleSizeChange (val) {
+      this.$emit('page-size', val)
+    },
+    handleCurrentChange (val) {
+      this.page = val
+      this.$emit('current-page', val)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.el-pagination {
+  white-space: nowrap;
+  text-align: right;
+  padding: 6px 5px;
+  color: #303133;
+  font-weight: 700;
+}
+</style>
